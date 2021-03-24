e08c3c601127b76b1562f4188802c71ec38d329b
lawauditswe <64290055+lawauditswe@users.noreply.github.com>
2021-03-24 21:24:22 +0000

    Add files via upload

---
+++ pokemon.py
@@ -0,0 +1,11 @@
+from Pokemon_class import *
+
+# Your Pokemon
+pikachu = Pokemon('Pikachu', 'modest', 'lightning rod', 'electric', thunderbolt, 35, 55, 40, 50, 50, 90, 0, 252, 4, 252, 0, 0, 30, 31, 31, 31, 31, 31, 'Male', 100, None, surf, quick_attack, iron_tail)
+staryu = Pokemon('Staryu', 'timid', 'analytic', 'water', hydro_pump, 30, 45, 55, 70, 55, 85, 0, 0, 4, 252, 0, 252, 31, 0, 31, 31, 31, 31, None, 100, None, thunder, ice_beam, psychic)
+bulbasaur = Pokemon('Bulbasaur', 'lonely', 'overgrow', 'grass', power_whip, 45, 49, 49, 65, 65, 45, 0, 252, 0, 252, 4, 0, 31, 31, 31, 31, 31, 31, 'Female', 100, 'poison', tackle, sludge_bomb, rock_smash)
+
+# Computer's Pokemon
+geodude = Pokemon('Geodude', 'brave', 'sturdy', 'rock', earthquake, 40, 80, 100, 30, 30, 20, 252, 252, 4, 0, 0, 0, 31, 31, 31, 31, 31, 31, 'Male', 100, 'ground', stone_edge, thunder_punch, fire_punch)
+dratini = Pokemon('Dratini', 'quiet', 'shed skin', 'dragon', waterfall, 41, 64, 45, 50, 50, 50, 0, 252, 0, 252, 4, 0, 31, 31, 31, 31, 31, 31, 'Female', 100, None, extreme_speed, draco_meteor, fire_blast)
+ponyta = Pokemon('Ponyta', 'naughty', 'flame body', 'fire', flare_blitz, 50, 85, 55, 65, 65, 90, 0, 252, 4, 0, 0, 252, 31, 31, 31, 31, 31, 31, 'Female', 100, None, high_horsepower, wild_charge, flamethrower)
