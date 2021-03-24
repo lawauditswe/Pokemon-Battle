d0114845faf31448df70ef6b674e662016a3da40
lawauditswe <64290055+lawauditswe@users.noreply.github.com>
2021-03-24 21:33:59 +0000

    Add files via upload

---
+++ Move_class.py
@@ -0,0 +1,12 @@
+# Move Class
+
+from Pokemon_class import *
+
+class Move:
+    def __init__(self, name, typing, category, power, accuracy):
+    	self.name = name
+        self.typing = typing
+        self.category = category
+        self.power = power
+        self.accuracy = accuracy
+        self.pp = pp
---
+++ moves.py
@@ -0,0 +1,37 @@
+# Moves
+
+from Move_class import *
+
+thunderbolt = Move('Thunderbolt', 'electric', 'special', 90, 100, 24)
+surf = Move('Surf', 'water', 'special', 90, 100, 24)
+quick_attack = Move('Quick Attack', 'normal', 'physical', 40, 100, 48)
+iron_tail = Move('Iron Tail', 'steel', 'physical', 100, 75, 24)
+
+hydro_pump = Move('Hydro Pump', 'water', 'special', 110, 80, 8)
+thunder = Move('Thunder', 'electric', 'special', 110, 70, 16)
+ice_beam = Move('Ice Beam', 'ice', 'special', 90, 100, 16)
+psychic = Move('Psychic', 'psychic', 'special', 90, 100, 16)
+
+power_whip = Move('Power Whip', 'grass', 'physical', 120, 85, 16)
+tackle = Move('Tackle', 'normal', 'physical', 40, 100, 56)
+sludge_bomb = Move('Sludge Bomb', 'poison', 'special', 90, 100, 16)
+rock_smash = Move('Rock Smash', 'fighting', 'physical', 40, 100, 24)
+
+earthquake = Move('Earthquake', 'ground', 'physical', 100, 100, 16)
+stone_edge = Move('Stone Edge', 'rock', 'physical', 100, 80, 8)
+thunder_punch = Move('Thunder Punch', 'electric', 'physical', 75, 100, 24)
+fire_punch = Move('Fire Punch', 'fire', 'physical', 75, 100, 24)
+
+waterfall = Move('Waterfall', 'water', 'physical', 80, 100, 24)
+extreme_speed = Move('Extreme Speed', 'normal', 'physical', 80, 100, 24)
+draco_meteor = Move('Draco Meteor', 'dragon', 'special', 130, 90, 8)
+fire_blast = Move('Fire Blast', 'fire', 'special', 110, 85, 8)
+
+flare_blitz = Move('Fire Blitz', 'fire', 'physical', 120, 100, 24)
+high_horsepower = Move('High Horsepower', 'ground', 'physical', 95, 95, 16)
+wild_charge = Move('Wild Charge', 'electric', 'physical', 90, 100, 24)
+flamethrower = Move('Flamethrower', 'fire', 'special', 90, 100, 24)
+
+
+
+
